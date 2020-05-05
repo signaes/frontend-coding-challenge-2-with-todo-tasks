82d0b598e0f4a78e2d85b7cb6bbd4c3ef996a8d6
Thiago Oliveira <thiago@signaes.com>
2020-05-05 16:57:03 -0300

    Add FETCHING and INCREMENT actions

--- src/redux/modules/questions.js
+++ src/redux/modules/questions.js
@@ -2,18 +2,53 @@ import api from '../../api.js';
 
 // Actions
 const GET_SUCCESS  = 'questions/GET_SUCCESS';
+const FETCHING  = 'questions/FETCHING';
+const INCREMENT = 'questions/INCREMENT';
 
 // Reducer
 const initialState = {
   list: null,
-  error: null
+  error: null,
+  fetching: false
 }
 
 export default function reducer (state = initialState, action = {}) {
   switch (action.type) {
+    case FETCHING:
+      return {
+        ...state,
+        fetching: true
+      };
     case GET_SUCCESS:
       return {...state,
         list: action.questions,
+        fetching: false
+      };
+    case INCREMENT:
+      return {
+        ...state,
+        list: state.list.map(question => {
+          const { votes, url } = action.payload
+
+          if (url.indexOf(question.url) === -1) {
+            return question;
+          }
+
+          return {
+            ...question,
+            choices: question.choices.map(currentChoice => {
+              if (currentChoice.url !== url) {
+                return currentChoice;
+              }
+
+              return {
+                ...currentChoice,
+                votes
+              };
+            }),
+          }
+        }),
+        fetching: false
       };
     default: return state;
   }
@@ -26,6 +61,11 @@ export const getQuestions = () => (dispatch) => {
 }
 
 export const vote = (url) => (dispatch) => {
-    api.choices.vote(url)
-      .then(() => dispatch(getQuestions()));
+    dispatch({ type: FETCHING });
+
+    api.choices.vote(url).then((response) => {
+      const { data: { votes, url } } = response;
+
+      dispatch({ type: INCREMENT, payload: { votes, url } });
+    });
 }
