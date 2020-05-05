c6764f139a481326dc2dbb92e3b13f279a73d89c
Thiago Oliveira <thiago@signaes.com>
2020-05-05 17:16:29 -0300

    Use a stateless component

--- src/components/Choices.js
+++ src/components/Choices.js
@@ -1,43 +1,44 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 
-export class Choices extends PureComponent {
-  render () {
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Choices</th>
-            <th>Votes</th>
-            <th>Percentage</th>
-            {this.props.onVote &&
-              <th></th>
+const Choices = ({
+  onVote,
+  choices,
+  totalVotes,
+  fetching
+}) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Choices</th>
+        <th>Votes</th>
+        <th>Percentage</th>
+        {onVote &&
+          <th></th>
+        }
+      </tr>
+    </thead>
+    <tbody>
+      {choices.map(({choice, url, votes}) => {
+        const percentage = totalVotes === 0 ? 0 : Math.round(votes / totalVotes * 100);
+        return (
+          <tr key={`${choice}-${url}`}>
+            <td>{choice}</td>
+            <td>{votes || 0}</td>
+            <td>{percentage}%</td>
+            {onVote &&
+              <td>
+                <button
+                  disabled={fetching}
+                  onClick={() => onVote(url)}>
+                  Vote
+                </button>
+              </td>
             }
           </tr>
-        </thead>
-        <tbody>
-          {this.props.choices.map(({choice, url, votes}) => {
-            const percentage = Math.round(votes / this.props.totalVotes * 100);
-            return (
-              <tr key={url}>
-                <td>{choice}</td>
-                <td>{votes || 0}</td>
-                <td>{percentage}%</td>
-                {this.props.onVote &&
-                  <td>
-                    <button
-                      disabled={this.props.fetching}
-                      onClick={() => this.props.onVote(url)}>
-                      Vote
-                    </button>
-                  </td>
-                }
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    )
-  }
-}
+        )
+      })}
+    </tbody>
+  </table>
+);
 
 export default Choices;
